be26e64af426e57bb9caa4af1ef2da91c5c30766
Harald Barth <haba@kth.se>
2020-04-19 01:22:23 +0200

    default off

--- DCCpp_Uno/Config.h
+++ DCCpp_Uno/Config.h
@@ -66,7 +66,7 @@ Part of DCC++ BASE STATION for the Arduino
 // The value of the TRIGGERPIN is defined in DCCpp_Uno.h because it might
 // be board specific
 //
-#define USE_TRIGGERPIN
+//#define USE_TRIGGERPIN
 //#define USE_TRIGGERPIN_PER_BIT
 
 /////////////////////////////////////////////////////////////////////////////////////
@@ -98,6 +98,7 @@ Part of DCC++ BASE STATION for the Arduino
 // circuits. 1 means shutdown at first over current occurence.
 //
 #define SHORT_SENSITIVITY 2
+
 /////////////////////////////////////////////////////////////////////////////////////
 //
 // RAILCOM_CUTOUT: If you want to generate a railcom cutout. Experimental!
