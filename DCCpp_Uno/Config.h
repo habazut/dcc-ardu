60f60b64f73ba8421464a9a20d05f1e7c4bbab02
Harald Barth <haba@kth.se>
2020-04-19 03:53:09 +0200

    Seen RailCom data

--- DCCpp_Uno/Config.h
+++ DCCpp_Uno/Config.h
@@ -66,7 +66,7 @@ Part of DCC++ BASE STATION for the Arduino
 // The value of the TRIGGERPIN is defined in DCCpp_Uno.h because it might
 // be board specific
 //
-//#define USE_TRIGGERPIN
+#define USE_TRIGGERPIN
 //#define USE_TRIGGERPIN_PER_BIT
 
 /////////////////////////////////////////////////////////////////////////////////////
@@ -103,5 +103,4 @@ Part of DCC++ BASE STATION for the Arduino
 //
 // RAILCOM_CUTOUT: If you want to generate a railcom cutout. Experimental!
 //
-//#define RAILCOM_CUTOUT
-
+#define RAILCOM_CUTOUT
